61788ab7fa0d6325798574cbef8668d6f99bc432
Kunal Chauhan <kunalchauhanlion@gmail.com>
2024-06-29 19:56:42 +0530

    done making the login page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import Chat from "./Components/chats/Chat";
 import Details from "./Components/detail/details";
 import List from "./Components/list/List";
+import Login from "./Components/Login/Login";
+
+const user = false
+
 function App() {
   return (
     <div className="container">
-      <List/>
-      <Chat/>
-      <Details/>
+      {user?(
+        <>
+          <List/>
+          <Chat/>
+          <Details/>
+        </>
+      ):(
+        <Login/>
+      )}
     </div>
   );
 }
---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import './Login.css'
+const Login = () => {
+    const [avatar,setAvatar] = useState({
+        file:null,
+        url:""
+    })
+    const handelAvatar = (e)=>{
+        if(e.target.files[0]){
+            setAvatar({
+                file:e.target.files[0],
+                url:URL.createObjectURL(e.target.files[0])
+            })
+        }
+    }
+    return (
+        <div className="login">
+            <div className="item">
+                <h2>Welcome Back,</h2>
+                <form>
+                    <input type="email" placeholder='Email' name='email' />
+                    <input type="password" placeholder='Password' name='password' />
+                    <button>Sign In</button>
+                </form>
+            </div>
+            <div className="separator"></div>
+            <div className="item">
+            <h2>Create An Account</h2>
+                <form>
+                    <img src={avatar.url||"./assets/avatar.png"} alt="" srcset="" />
+                    <label htmlFor="file">Upload an Image
+                    </label>
+                    <input type="file" id="file"  style={{display:'none'}} onChange={handelAvatar}/>
+                    <input type="text" placeholder='Username' name='username' />
+                    <input type="email" placeholder='Email' name='email' />
+                    <input type="password" placeholder='Password' name='password' />
+                    <button>Sign Up</button>
+                </form>
+            </div>
+        </div>
+    )
+}
+export default Login;
